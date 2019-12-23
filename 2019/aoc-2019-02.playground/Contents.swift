15a3e5f40ad803a9b90ae3372f71c2c063c72219
cor <cor@pruijs.nl>
2019-12-23 16:25:58 +0100

    Add second part of 2019-02

--- 2019/aoc-2019-02.playground/Contents.swift
+++ 2019/aoc-2019-02.playground/Contents.swift
@@ -21,27 +21,28 @@
 
 // increase program counter by 4 after executing instruction
 
+// MARK: - PART ONE
 typealias IntcodeProgram = [Int]
 
 func executeIntcode(program inputProgram: IntcodeProgram) -> IntcodeProgram {
     var program = inputProgram
-    var programCounter = 0
+    var instructionPointer = 0
     var running = true
 
-    while (running && programCounter < program.count)
+    while (running && instructionPointer < program.count)
     {
-        let opcode = program[programCounter]
+        let opcode = program[instructionPointer]
 
         switch opcode {
         case 1: // Add
-            let inputAddress1 = program[programCounter + 1]
-            let inputAddress2 = program[programCounter + 2]
-            let outputAddress = program[programCounter + 3]
+            let inputAddress1 = program[instructionPointer + 1]
+            let inputAddress2 = program[instructionPointer + 2]
+            let outputAddress = program[instructionPointer + 3]
             program[outputAddress] = program[inputAddress1] + program[inputAddress2]
         case 2: // Multiply
-            let inputAddress1 = program[programCounter + 1]
-            let inputAddress2 = program[programCounter + 2]
-            let outputAddress = program[programCounter + 3]
+            let inputAddress1 = program[instructionPointer + 1]
+            let inputAddress2 = program[instructionPointer + 2]
+            let outputAddress = program[instructionPointer + 3]
             program[outputAddress] = program[inputAddress1] * program[inputAddress2]
         case 99: // Exit
             running = false
@@ -50,7 +51,7 @@ func executeIntcode(program inputProgram: IntcodeProgram) -> IntcodeProgram {
             running = false
         }
         
-        programCounter += 4 // Go to next instruction
+        instructionPointer += 4 // Go to next instruction
     }
     
     return program
@@ -58,11 +59,34 @@ func executeIntcode(program inputProgram: IntcodeProgram) -> IntcodeProgram {
 
 // Example Inputs
 executeIntcode(program: [1, 0, 0, 0, 99])
+
 executeIntcode(program: [2, 3, 0, 3, 99])
 executeIntcode(program: [2, 4, 4, 5, 99, 0])
 executeIntcode(program: [1, 1, 1, 4, 99, 5, 6, 0, 99])
 
+
 let challangeInput = [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,13,19,1,9,19,23,2,13,23,27,2,27,13,31,2,31,10,35,1,6,35,39,1,5,39,43,1,10,43,47,1,5,47,51,1,13,51,55,2,55,9,59,1,6,59,63,1,13,63,67,1,6,67,71,1,71,10,75,2,13,75,79,1,5,79,83,2,83,6,87,1,6,87,91,1,91,13,95,1,95,13,99,2,99,13,103,1,103,5,107,2,107,10,111,1,5,111,115,1,2,115,119,1,119,6,0,99,2,0,14,0]
 
 // First Half Answer
-executeIntcode(program: challangeInput)
+executeIntcode(program: challangeInput)[0]
+
+// MARK: - PART TWO
+func find19690720combo () -> Int? {
+    // Brute force
+    for noun in 0...99 {
+        for verb in 0...99 {
+            var challangeInputCopy = challangeInput
+            challangeInputCopy[1] = noun
+            challangeInputCopy[2] = verb
+            
+            if executeIntcode(program: challangeInputCopy)[0] == 19690720 {
+                // Ladies and gentlemen, we got him.
+                return 100 * noun + verb
+            }
+        }
+    }
+    return nil // The challange is broken
+}
+
+// Second Half Answer
+find19690720combo()!
