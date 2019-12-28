095f234c42aeec2b12cbc3ddf5253183c650169c
cor <cor@pruijs.nl>
2019-12-28 17:21:37 +0100

    Refactor parameter decoding

--- 2019/aoc-2019-05.playground/Contents.swift
+++ 2019/aoc-2019-05.playground/Contents.swift
@@ -23,6 +23,7 @@ import Foundation
 
 // increase program counter by 4 after executing instruction
 
+
 // MARK: - Int Extensions
 typealias IntcodeProgram = [Int]
 
@@ -47,45 +48,36 @@ extension Int {
     
     // MARK: Machine state
     var program = inputProgram
-    var instructionPointer = 0
+    var IP = 0
     var running = true
 
-    while (running && instructionPointer < program.count)
+    while (running && IP < program.count)
     {
-        let opcode = program[instructionPointer]
+        let opcode = program[IP]
 
         switch opcode.digits(2) {
         
         // MARK: Add
         case 1:
-            let input1 = program[instructionPointer + 1]
-            let input2 = program[instructionPointer + 2]
-            let output = program[instructionPointer + 3]
+            let parameters = decodeParameters(program, IP, amount: 3, raw: [2])
             
-            let operand1 = opcode.digit(2) == 1 ? input1 : program[input1]
-            let operand2 = opcode.digit(3) == 1 ? input2 : program[input2]
+            program[parameters[2]] = parameters[0] + parameters[1]
             
-            program[output] = operand1 + operand2
-            
-            instructionPointer += 4
+            IP += 4
             
         // MARK: Multiply
         case 2:
-            let input1 = program[instructionPointer + 1]
-            let input2 = program[instructionPointer + 2]
-            let output = program[instructionPointer + 3]
+            let parameters = decodeParameters(program, IP, amount: 3, raw: [2])
+                        
+            program[parameters[2]] = parameters[0] * parameters[1]
             
-            let operand1 = opcode.digit(2) == 1 ? input1 : program[input1]
-            let operand2 = opcode.digit(3) == 1 ? input2 : program[input2]
-            
-            program[output] = operand1 * operand2
-            
-            instructionPointer += 4
+            IP += 4
             
         // MARK: Input
         case 3:
-            let outputAddress = program[instructionPointer + 1]
+            let outputAddress = program[IP + 1]
     
+            // TEMP
             var userInput: Int? = nil
             
             for i in (1...5).reversed() {
@@ -101,7 +93,7 @@ extension Int {
                     }
                 }
             }
-            
+
             if let input = userInput {
                 program[outputAddress] = input
             } else {
@@ -109,15 +101,47 @@ extension Int {
                 running = false
             }
             
-            instructionPointer += 2
+            IP += 2
     
         // MARK: Output
         case 4:
-            let input = program[instructionPointer + 1]
+            let input = program[IP + 1]
             
             print(program[input])
             
-            instructionPointer += 2
+            IP += 2
+            
+//        // MARK: Jump-if-true
+//        case 5:
+//            let input1 = program[instructionPointer + 1]
+//            let input2 = program[instructionPointer + 2]
+//
+//            let parameter1 = opcode.digit(2) == 1 ? input1 : program[input1]
+//            let parameter2 = opcode.digit(3) == 1 ? input2 : program[input2]
+//
+//
+//            if parameter1 != 0 {
+//                instructionPointer = parameter2 // Jump
+//            } else {
+//                instructionPointer += 3
+//            }
+//
+//
+//        // MARK: Jump-if-false
+//        case 6:
+//            let input1 = program[instructionPointer + 1]
+//            let input2 = program[instructionPointer + 2]
+//
+//            let parameter1 = opcode.digit(2) == 1 ? input1 : program[input1]
+//            let parameter2 = opcode.digit(3) == 1 ? input2 : program[input2]
+//
+//
+//            if parameter1 == 0 {
+//                instructionPointer = parameter2 // Jump
+//            } else {
+//                instructionPointer += 3
+//            }
+//
             
         // MARK: Exit
         case 99:
@@ -133,11 +157,28 @@ extension Int {
     return program
 }
 
+func decodeParameters(_ program: IntcodeProgram, _ IP: Int, amount: Int, raw: [Int]) -> [Int] {
+    return (1...amount).map { i in
+        let input = program[IP + i]
+        
+        if program[IP].digit(i + 1) == 1 {
+            // Immediate mode
+            return input
+        } else if raw.contains(i - 1) {
+            // Raw input
+            return input
+        } else {
+            return program[input]
+        }
+    }
+}
+
 // MUST BE RUN FROM TERMINAL since readLine does not work in playgrounds
 let challangeProgram = [3, 225, 1, 225, 6, 6, 1100, 1, 238, 225, 104, 0, 1001, 191, 50, 224, 101, -64, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 5, 224, 224, 1, 224, 223, 223, 2, 150, 218, 224, 1001, 224, -1537, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 2, 224, 1, 223, 224, 223, 1002, 154, 5, 224, 101, -35, 224, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 5, 224, 1, 224, 223, 223, 1102, 76, 17, 225, 1102, 21, 44, 224, 1001, 224, -924, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 4, 224, 1, 224, 223, 223, 101, 37, 161, 224, 101, -70, 224, 224, 4, 224, 1002, 223, 8, 223, 101, 6, 224, 224, 1, 223, 224, 223, 102, 46, 157, 224, 1001, 224, -1978, 224, 4, 224, 102, 8, 223, 223, 1001, 224, 5, 224, 1, 224, 223, 223, 1102, 5, 29, 225, 1101, 10, 7, 225, 1101, 43, 38, 225, 1102, 33, 46, 225, 1, 80, 188, 224, 1001, 224, -73, 224, 4, 224, 102, 8, 223, 223, 101, 4, 224, 224, 1, 224, 223, 223, 1101, 52, 56, 225, 1101, 14, 22, 225, 1101, 66, 49, 224, 1001, 224, -115, 224, 4, 224, 1002, 223, 8, 223, 1001, 224, 7, 224, 1, 224, 223, 223, 1101, 25, 53, 225, 4, 223, 99, 0, 0, 0, 677, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1105, 0, 99999, 1105, 227, 247, 1105, 1, 99999, 1005, 227, 99999, 1005, 0, 256, 1105, 1, 99999, 1106, 227, 99999, 1106, 0, 265, 1105, 1, 99999, 1006, 0, 99999, 1006, 227, 274, 1105, 1, 99999, 1105, 1, 280, 1105, 1, 99999, 1, 225, 225, 225, 1101, 294, 0, 0, 105, 1, 0, 1105, 1, 99999, 1106, 0, 300, 1105, 1, 99999, 1, 225, 225, 225, 1101, 314, 0, 0, 106, 0, 0, 1105, 1, 99999, 108, 226, 226, 224, 1002, 223, 2, 223, 1005, 224, 329, 101, 1, 223, 223, 108, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 344, 1001, 223, 1, 223, 8, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 359, 101, 1, 223, 223, 7, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 374, 101, 1, 223, 223, 107, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 389, 101, 1, 223, 223, 7, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 404, 1001, 223, 1, 223, 1107, 677, 226, 224, 1002, 223, 2, 223, 1006, 224, 419, 1001, 223, 1, 223, 1007, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 434, 101, 1, 223, 223, 1008, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 449, 1001, 223, 1, 223, 1007, 677, 677, 224, 1002, 223, 2, 223, 1006, 224, 464, 1001, 223, 1, 223, 1008, 226, 226, 224, 102, 2, 223, 223, 1006, 224, 479, 101, 1, 223, 223, 1007, 226, 677, 224, 1002, 223, 2, 223, 1005, 224, 494, 1001, 223, 1, 223, 108, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 509, 101, 1, 223, 223, 8, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 524, 1001, 223, 1, 223, 107, 677, 677, 224, 1002, 223, 2, 223, 1005, 224, 539, 101, 1, 223, 223, 107, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 554, 101, 1, 223, 223, 1107, 226, 677, 224, 1002, 223, 2, 223, 1006, 224, 569, 1001, 223, 1, 223, 1108, 677, 226, 224, 102, 2, 223, 223, 1005, 224, 584, 1001, 223, 1, 223, 1008, 677, 677, 224, 102, 2, 223, 223, 1005, 224, 599, 1001, 223, 1, 223, 1107, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 614, 101, 1, 223, 223, 7, 226, 226, 224, 102, 2, 223, 223, 1005, 224, 629, 1001, 223, 1, 223, 1108, 677, 677, 224, 102, 2, 223, 223, 1006, 224, 644, 1001, 223, 1, 223, 8, 677, 226, 224, 1002, 223, 2, 223, 1005, 224, 659, 101, 1, 223, 223, 1108, 226, 677, 224, 102, 2, 223, 223, 1005, 224, 674, 101, 1, 223, 223, 4, 223, 99, 226]
 
 
+
+executeIntcode(program: [1101, 100, -1, 4, 0])
+
 // First Half Answer
 executeIntcode(program: challangeProgram)
-
-
