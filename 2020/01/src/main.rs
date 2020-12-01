b02b21ed94e2203be552b35e0db6cd1f5add1aae
cor <cor@pruijs.nl>
2020-12-01 09:47:43 +0100

    Read numbers from input

--- 2020/01/src/main.rs
+++ 2020/01/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, ErrorKind, Read};
+
+fn read<R: Read>(io: R) -> Result<Vec<i64>, Error> {
+    let br = BufReader::new(io);
+    br.lines()
+        .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("./input/01.txt").expect("Failed to open file");
+    let vec = read(file).expect("Failed to read file");
+
+    println!("{}", vec[0]);
 }
