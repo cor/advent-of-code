e06db47f0e4f8f54c3079d91c1005fc59d62e0eb
cor <cor@pruijs.nl>
2020-12-03 12:37:45 +0100

    Finish 2020/03 part 1

--- 2020/03/src/main.rs
+++ 2020/03/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error, Read};
+use std::thread::current;
 
 fn read_lines<R: Read>(io: R) -> Result<Vec<String>, Error> {
     BufReader::new(io).lines().collect()
@@ -11,12 +12,29 @@ struct World {
     source: Vec<Square>,
 }
 
-impl World {
-    fn square_at(&self, x: usize, y: usize) -> &Square {
-        let x = x % self.width; // repeat to the right
-        &self.source[x + y * self.width]
+#[derive(Debug)]
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+impl Point {
+    fn start() -> Point {
+        Point {
+            x: 0,
+            y: 0,
+        }
+    }
+
+    fn add_slope(&self, slope: &Slope) -> Point {
+        Point {
+            x: self.x + slope.x,
+            y: self.y + slope.y,
+        }
     }
 }
+
+type Slope = Point;
 
 #[derive(Debug)]
 enum Square {
@@ -25,6 +43,26 @@ enum Square {
 }
 
 impl World {
+    fn square_at(&self, point: &Point) -> &Square {
+        let x = point.x % self.width; // repeat to the right
+        &self.source[x + point.y * self.width]
+    }
+
+    fn count_trees_with_slope(&self, slope: &Slope) -> usize {
+        let mut tree_count = 0;
+        let mut current_point = Point::start();
+
+        while current_point.y < self.height{
+            tree_count += match self.square_at(&current_point) {
+                Square::Open => 0,
+                Square::Tree => 1,
+            };
+            current_point = current_point.add_slope(slope);
+        }
+
+        tree_count
+    }
+
     fn from_lines(lines: &Vec<String>) -> World {
         let width = lines[0].len();
         let height = lines.len();
@@ -53,8 +91,8 @@ impl World {
 fn main() {
     let file = File::open("./input/1.txt").expect("Failed to open file");
     let lines = read_lines(file).expect("Incorrect input");
-
     let world = World::from_lines(&lines);
 
-    println!("{:#?}", world.square_at(36, 5));
+    let slope = Slope { x: 3, y: 1 };
+    println!("{:#?}", world.count_trees_with_slope(&slope));
 }
