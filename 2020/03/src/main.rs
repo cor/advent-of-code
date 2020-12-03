bd562c5c3af7abafd9a8ecb68ad32c53f3a589e3
cor <cor@pruijs.nl>
2020-12-03 12:13:37 +0100

    Parse input

--- 2020/03/src/main.rs
+++ 2020/03/src/main.rs
@@ -1,3 +1,53 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, Read};
+
+fn read_lines<R: Read>(io: R) -> Result<Vec<String>, Error> {
+    BufReader::new(io).lines().collect()
+}
+#[derive(Debug)]
+struct World {
+    width: usize,
+    height: usize,
+    source: Vec<Square>,
+}
+
+#[derive(Debug)]
+enum Square {
+    Open,
+    Tree,
+}
+
+impl World {
+    fn from_lines(lines: &Vec<String>) -> World {
+        let width = lines[0].len();
+        let height = lines.len();
+        let mut source = Vec::with_capacity(width * height);
+
+        for line in lines {
+            for square in line.as_bytes() {
+                let square = match square {
+                    b'.' => Square::Open,
+                    b'#' => Square::Tree,
+                    _    => panic!("Invalid character in input"),
+                };
+                source.push(square);
+            }
+        }
+
+        World {
+            width,
+            height,
+            source,
+        }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("./input/1.txt").expect("Failed to open file");
+    let lines = read_lines(file).expect("Incorrect input");
+
+    let world = World::from_lines(&lines);
+
+    println!("{:?}", world.source);
 }
