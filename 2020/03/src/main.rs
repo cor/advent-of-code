0e94c077f758af7f1d5bb36aa82c4fb04cc26dc3
cor <cor@pruijs.nl>
2020-12-04 02:32:31 +0100

    Implement FromStr for World

--- 2020/03/src/main.rs
+++ 2020/03/src/main.rs
@@ -1,9 +1,14 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader, Error, Read};
+use std::io::{Read};
 use std::ops::{AddAssign, Index};
+use std::str::FromStr;
 
-fn read_lines<R: Read>(io: R) -> Result<Vec<String>, Error> {
-    BufReader::new(io).lines().collect()
+fn load_file(path: &str) -> String {
+    let mut input = String::new();
+    let mut f = File::open(path).expect("Unable to open file");
+    f.read_to_string(&mut input).expect("Unable to read string");
+
+    input
 }
 
 #[derive(Debug)]
@@ -40,6 +45,10 @@ struct World {
 }
 
 impl World {
+    fn point_in_bounds(&self, point: &Point) -> bool {
+        point.y < self.height
+    }
+
     fn count_trees_with(&self, slope: &Slope) -> usize {
         let mut current_point = Point::START;
         let mut tree_count = 0;
@@ -54,12 +63,13 @@ impl World {
 
         tree_count
     }
+}
 
-    fn point_in_bounds(&self, point: &Point) -> bool {
-        point.y < self.height
-    }
+impl FromStr for World {
+    type Err = String;
 
-    fn from_lines(lines: &Vec<String>) -> World {
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let lines: Vec<&str> = s.lines().collect();
         let width = lines[0].len();
         let height = lines.len();
         let mut source = Vec::with_capacity(width * height);
@@ -75,11 +85,11 @@ impl World {
             }
         }
 
-        World {
+        Ok(World {
             width,
             height,
             source,
-        }
+        })
     }
 }
 
@@ -92,9 +102,8 @@ impl Index<&Point> for World {
 }
 
 fn main() {
-    let file = File::open("./input/1.txt").expect("Failed to open file");
-    let lines = read_lines(file).expect("Incorrect input");
-    let world = World::from_lines(&lines);
+    let input = load_file("./input/1.txt");
+    let world = World::from_str(&input).expect("Couldn't parse world.");
 
     // Part 1
     let answer1 = world.count_trees_with(&Slope { x: 3, y: 1 });
