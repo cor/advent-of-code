554cdbf79a6baad4469eb0eba4c4a24314e42b0f
cor <cor@pruijs.nl>
2020-12-05 17:40:26 +0100

    Add Seat, read input

--- 2020/05/src/main.rs
+++ 2020/05/src/main.rs
@@ -2,6 +2,26 @@ use std::fs::File;
 use std::io::Read;
 use std::str::FromStr;
 
+#[derive(Debug)]
+struct Seat {
+    row: usize,
+    column: usize,
+}
+
+impl Seat {
+    fn id(&self) -> usize {
+        self.row * 8 + self.column
+    }
+}
+
+impl FromStr for Seat {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Seat { row: 4, column: 4 }) // TODO: Calculate based on s
+    }
+}
+
 
 fn load_file(path: &str) -> String {
     let mut input = String::new();
@@ -14,5 +34,18 @@ fn load_file(path: &str) -> String {
 fn main() {
     let input = load_file("./input/1.txt");
 
-    println!("{}", input);
+    let seats: Vec<Seat> = input
+        .lines()
+        .map(|s| Seat::from_str(s))
+        .filter_map(Result::ok)
+        .collect();
+
+    let a_seat = Seat {
+        row: 70,
+        column: 7,
+    };
+
+    println!("{:?}", a_seat);
+    println!("{:?}", seats);
+    println!("{:?}", a_seat.id());
 }
