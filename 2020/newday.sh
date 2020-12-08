4cb9f16dafb71a59cd754e3fe3bc126d78f201ba
cor <cor@pruijs.nl>
2020-12-08 20:42:27 +0100

    Automate project setup

--- 2020/newday.sh
+++ 2020/newday.sh
@@ -4,3 +4,15 @@ cargo new $1 --name="aoc-2020-$1"
 
 echo "aoc-2020-common = { path = \"../common/\" }" >> ./$1/Cargo.toml
 
+mkdir $1/input
+
+touch $1/input/1.txt
+
+echo 'use aoc_2020_common::common::load_file;
+
+fn main() {
+    let input = load_file("./input/1.txt");
+    println!("{}", input);
+}' > ./$1/src/main.rs
+
+clion $1
