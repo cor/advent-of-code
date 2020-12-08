5e9535e021b68e96744e8b0ada2e0fe21eb9683b
cor <cor@pruijs.nl>
2020-12-08 21:58:33 +0100

    Finish 2020/08 part 1

--- 2020/08/src/main.rs
+++ 2020/08/src/main.rs
@@ -2,23 +2,29 @@ use aoc_2020_common::common::load_file;
 use regex::Regex;
 
 #[derive(Debug)]
-enum Instruction {
+enum Operation {
     NOP,
     ACC,
     JMP,
 }
 
-fn main() {
-    let input = load_file("./input/1.txt");
+#[derive(Debug)]
+struct MachineState {
+    accumulator: i64,
+    program_counter: i64,
+    running: bool,
+}
+
+fn parse_instructions(input: &str) -> Vec<(Operation, i64)> {
     let instruction_re: Regex = Regex::new(r#"([a-z]{3}) ([+\-])(\d+)"#).unwrap();
 
-    let instructions: Vec<(Instruction, i64)> = instruction_re
+    instruction_re
         .captures_iter(&input)
         .map(|cap| {
-            let instruction = match &cap[1] {
-                "nop" => Instruction::NOP,
-                "acc" => Instruction::ACC,
-                "jmp" => Instruction::JMP,
+            let operation = match &cap[1] {
+                "nop" => Operation::NOP,
+                "acc" => Operation::ACC,
+                "jmp" => Operation::JMP,
                 _ => panic!("Invalid instruction")
             };
             let argument = match &cap[2] {
@@ -27,9 +33,41 @@ fn main() {
                 _ => panic!("Invalid sign before argument"),
             };
 
-            (instruction, argument)
+            (operation, argument)
         })
-        .collect();
+        .collect()
+}
 
-    println!("{:#?}", instructions);
+fn main() {
+    let input = load_file("./input/1.txt");
+    let instructions = parse_instructions(&input);
+
+    let mut st = MachineState { accumulator: 0, program_counter: 0, running: true };
+    let mut visited_instructions: Vec<usize> = Vec::new();
+
+    while st.running {
+        let pc = st.program_counter as usize;
+
+        // Terminate before running an instruction a second time
+        if visited_instructions.contains(&pc) {
+            st.running = false;
+            break;
+        }
+
+        // Mark instruction as visited
+        visited_instructions.push(pc);
+
+        // Execute instruction
+        match &instructions[pc] {
+            (Operation::NOP, _) => st.program_counter += 1,
+            (Operation::ACC, n) => { st.accumulator += n; st.program_counter += 1 }
+            (Operation::JMP, n) => st.program_counter += n,
+        }
+
+        // Check if we're at the end of our program
+        if st.program_counter >= instructions.len() as i64 {
+            st.running = false;
+        }
+    }
+    println!("{:?}", st.accumulator);
 }
