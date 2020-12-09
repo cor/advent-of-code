66770e448e14967b4e64972bc2251638d55bf35b
cor <cor@pruijs.nl>
2020-12-09 12:31:42 +0100

    Finish 2020/09 part 1

--- 2020/09/src/main.rs
+++ 2020/09/src/main.rs
@@ -1,5 +1,16 @@
 use aoc_2020_common::common::load_file;
 
+fn numbers_contain_summing_pair(numbers: &[usize], target: &usize) -> bool {
+    for (i, n) in numbers.iter().enumerate() {
+        for (j, m) in numbers.iter().enumerate() {
+            if i != j && n + m == *target {
+                return true
+            }
+        }
+    }
+    false
+}
+
 fn main() {
     let input = load_file("./input/1.txt");
     let numbers:Vec<usize> = input
@@ -7,5 +18,14 @@ fn main() {
         .map(|s| s.parse().unwrap())
         .collect();
 
-    println!("{:?}", numbers);
+    let range_size = 25;
+    
+    for (i, n) in numbers.iter().enumerate() {
+        if i > range_size {
+            let preceeding = &numbers[(i-range_size)..i];
+            if !numbers_contain_summing_pair(&preceeding, n) {
+                println!("{}", n);
+            }
+        }
+    }
 }
