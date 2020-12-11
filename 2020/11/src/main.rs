b023c756b11f94299be4f94aba9a5c63578a0a16
cor <cor@pruijs.nl>
2020-12-11 22:48:49 +0100

    Compute Area generations

--- 2020/11/src/main.rs
+++ 2020/11/src/main.rs
@@ -1,7 +1,7 @@
 use aoc_2020_common::common::load_file;
-use std::ops::Index;
+use std::ops::{Index, IndexMut};
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 enum Field {
     Floor,
     Seat,
@@ -18,7 +18,7 @@ impl Field {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Area {
     width: usize,
     height: usize,
@@ -45,6 +45,43 @@ impl Area {
             fields
         }
     }
+
+    fn next(&self) -> Area {
+        let mut a = self.clone();
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let point = (x,y);
+                if self[point] == Field::Floor { continue }
+                a[point] = match self.adjacent_occupied_count(&point) {
+                    0 => Field::Occupied,
+                    x if x >= 4 => Field::Seat,
+                    _ => continue,
+                }
+            }
+        }
+        a
+    }
+
+    fn in_bounds(&self, point: &(isize, isize)) -> bool {
+        let (x, y) = point;
+
+
+        *x > 0 && *x < self.width as isize && *y > 0 && *y < self.height as isize
+    }
+
+    fn adjacent_occupied_count(&self, point: &(usize, usize)) -> usize {
+        let (x, y) = (point.0 as isize, point.1 as isize);
+        [
+            (x-1, y-1), (x-1, y), (x-1, y+1),
+            (x, y-1), (x, y+1),
+            (x+1, y-1), (x+1, y), (x+1, y+1),
+        ].iter()
+            .filter(|&p| self.in_bounds(p))
+            .map(|&(x, y)| (x as usize, y as usize))
+            .filter(|&p| self[p] == Field::Occupied)
+            .count()
+    }
+
 }
 
 impl Index<(usize, usize)> for Area {
@@ -56,6 +93,16 @@ impl Index<(usize, usize)> for Area {
         }
 
         return &self.fields[index.0 + index.1 * self.width];
+    }
+}
+
+impl IndexMut<(usize, usize)> for Area {
+    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
+        if index.0 >= self.width || index.1 >= self.height {
+            panic!("Accessing out of range");
+        }
+
+        return &mut self.fields[index.0 + index.1 * self.width];
     }
 }
 
@@ -76,7 +123,11 @@ impl ToString for Area {
 
 
 fn main() {
-    let input = load_file("./input/1.txt");
+    let input = load_file("./input/example.txt");
     let area = Area::from(&input);
     println!("{}", area.to_string());
+
+    let area2 = area.next();
+    println!("{}", area2.to_string());
+
 }
