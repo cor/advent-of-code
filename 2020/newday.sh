7bb2be74295527701c8def08b5987c8bb08b976a
cor <cor@pruijs.nl>
2020-12-13 10:19:54 +0100

    Touch example input

--- 2020/newday.sh
+++ 2020/newday.sh
@@ -6,6 +6,7 @@ echo "aoc-2020-common = { path = \"../common/\" }" >> ./$1/Cargo.toml
 
 mkdir $1/input
 
+touch $1/input/example.txt
 touch $1/input/1.txt
 
 echo 'use aoc_2020_common::common::load_file;
