4f4c62e9cd0ae1e778463e737f44adb973226454
cor <cor@pruijs.nl>
2020-12-13 16:54:39 +0100

    Almost finish 2020/13 part 1

--- 2020/13/src/main.rs
+++ 2020/13/src/main.rs
@@ -3,11 +3,35 @@ use std::str::FromStr;
 use crate::Bus::Unavailable;
 use std::num::ParseIntError;
 
-
 #[derive(Debug)]
 enum Bus {
     Unavailable,
     ID(u64),
+}
+
+/// For use with functions on Option such as `filter_map`
+impl Bus {
+    fn available(&self) -> Option<u64> {
+        match self {
+            Bus::ID(n) => Some(*n),
+            Bus::Unavailable => None
+        }
+    }
+}
+
+/// Overly complex, but I wanted to try out making a custom Iterator
+#[derive(Debug)]
+struct BusIterator {
+    id: u64,
+    index: u64,
+}
+
+impl Iterator for BusIterator {
+    type Item = u64;
+    fn next(&mut self) -> Option<u64> {
+        self.index += 1;
+        Some(self.index * self.id)
+    }
 }
 
 impl FromStr for Bus {
@@ -29,10 +53,10 @@ impl FromStr for Bus {
 fn main() {
     let input = load_file("./input/1.txt");
     let mut lines = input.lines().into_iter();
-    println!("{}", input);
 
-    let departure_time = lines.next().unwrap().parse::<u64>();
+    let earliest_time = lines.next().unwrap().parse::<u64>().unwrap();
 
+    // Parse input
     let busses: Vec<Bus> = lines.next()
         .unwrap()
         .split(',')
@@ -40,5 +64,26 @@ fn main() {
         .filter_map(Result::ok)
         .collect();
 
-    dbg!(busses);
+    // Create an BusIterator for every bus that is available
+    let mut bus_iters: Vec<BusIterator> = busses
+        .iter()
+        .filter_map(Bus::available)
+        .map(|id| BusIterator {
+            id,
+            index: 0,
+        })
+        .collect();
+
+    // Find the earliest bus
+    let (bus_id, departure_time) = 'outer: loop {
+        for bus_iter in bus_iters.iter_mut() {
+            let time = bus_iter.next().unwrap();
+            if time >= earliest_time {
+                break 'outer (bus_iter.id, time);
+            }
+        }
+    };
+
+    // Part 1 answer
+    println!("{}", bus_id * (departure_time - earliest_time))
 }
