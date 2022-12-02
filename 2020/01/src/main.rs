e4625e2905c90dc8d77ae381b25fc0fd97ff9fea
cor <cor@pruijs.dev>
2022-12-02 14:30:00 +0100

    feat: finish day 1

--- 2020/01/src/main.rs
+++ 2020/01/src/main.rs
@@ -7,7 +7,7 @@ extern crate itertools;
 fn read_numbers<R: Read>(io: R) -> Result<Vec<i64>, Error> {
     let br = BufReader::new(io);
     br.lines()
-        .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
+        .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))a
         .collect()
 }
 
--- 2022/01/src/main.rs
+++ 2022/01/src/main.rs
@@ -3,18 +3,26 @@ use aoc_2022_common::challenge_input;
 fn main() {
     let calories = parse_input(&challenge_input());
 
-    println!("{}", part_1(&calories).expect("no groups in input"));
+    println!("{}", part_1(&calories));
+    println!("{}", part_2(calories));
 }
 
-fn parse_input(input: &str) -> Vec<Vec<u64>> {
+fn parse_input(input: &str) -> Vec<u64> {
     input
         .lines()
         .collect::<Vec<&str>>()
         .split(|l| l.is_empty())
-        .map(|g| g.iter().map(|s| s.parse::<u64>().unwrap()).collect())
+        .map(|g| g.iter().map(|s| s.parse::<u64>().unwrap()).sum())
         .collect()
 }
 
-fn part_1(calories: &[Vec<u64>]) -> Option<u64> {
-    calories.iter().map(|group| group.iter().sum()).max()
+fn part_1(calories: &[u64]) -> &u64 {
+    calories.iter().max().expect("no groups in input")
+}
+
+fn part_2(mut calories: Vec<u64>) -> u64 {
+    calories.sort_unstable();
+    calories.reverse();
+    calories.truncate(3);
+    calories.iter().sum()
 }
