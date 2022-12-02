a40ef154d7ccaa723c6111516f87770b2e4b67ce
cor <cor@pruijs.dev>
2022-12-02 14:07:54 +0100

    fix: simplify

--- 2022/01/src/main.rs
+++ 2022/01/src/main.rs
@@ -1,9 +1,9 @@
 use aoc_2022_common::challenge_input;
 
 fn main() {
-    let callories: Vec<Vec<u64>> = parse_input(&challenge_input());
+    let calories = parse_input(&challenge_input());
 
-    println!("{}", part_1(&callories).expect("no groups in input"));
+    println!("{}", part_1(&calories).expect("no groups in input"));
 }
 
 fn parse_input(input: &str) -> Vec<Vec<u64>> {
@@ -11,14 +11,10 @@ fn parse_input(input: &str) -> Vec<Vec<u64>> {
         .lines()
         .collect::<Vec<&str>>()
         .split(|l| l.is_empty())
-        .map(|g| {
-            g.iter()
-                .map(|s| s.parse::<u64>().expect("non-number line"))
-                .collect()
-        })
+        .map(|g| g.iter().map(|s| s.parse::<u64>().unwrap()).collect())
         .collect()
 }
 
-fn part_1(callories: &[Vec<u64>]) -> Option<u64> {
-    callories.iter().map(|group| group.iter().sum()).max()
+fn part_1(calories: &[Vec<u64>]) -> Option<u64> {
+    calories.iter().map(|group| group.iter().sum()).max()
 }
