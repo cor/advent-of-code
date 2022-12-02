1a622b8c34d70f9373f676d28c9c497a4c2f6588
cor <cor@pruijs.dev>
2022-12-02 12:06:52 +0100

    feat: read challenge input

--- 2022/01/src/main.rs
+++ 2022/01/src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::fs;
+
+fn challenge_input() -> String {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    fs::read_to_string(file_path).expect("Unable to read input file")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = challenge_input();
+    println!("Challenge input:\n{input}");
 }
