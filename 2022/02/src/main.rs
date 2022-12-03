53710ea610bcdfa63e822a8996b768626495634e
cor <cor@pruijs.dev>
2022-12-03 17:27:18 +0100

    fix: simplify 2022/02 part 1

--- 2022/02/src/main.rs
+++ 2022/02/src/main.rs
@@ -1,17 +1,17 @@
 use aoc_2022_common::challenge_input;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 enum Move {
-    Rock,
-    Paper,
-    Scissors,
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
 }
 
 #[derive(Debug)]
 enum Outcome {
-    Victory,
-    Draw,
-    Defeat,
+    Victory = 6,
+    Draw = 3,
+    Defeat = 0,
 }
 
 #[derive(Debug)]
@@ -32,26 +32,18 @@ fn part_1(game: &Game) -> u64 {
 
 impl Turn {
     pub fn outcome(&self) -> Outcome {
+        use Move::{Paper, Rock, Scissors};
+        use Outcome::{Defeat, Draw, Victory};
         match self {
-            Turn(Move::Rock, Move::Paper) => Outcome::Victory,
-            Turn(Move::Rock, Move::Scissors) => Outcome::Defeat,
-            Turn(Move::Paper, Move::Rock) => Outcome::Defeat,
-            Turn(Move::Paper, Move::Scissors) => Outcome::Victory,
-            Turn(Move::Scissors, Move::Paper) => Outcome::Defeat,
-            Turn(Move::Scissors, Move::Rock) => Outcome::Victory,
-            _ => Outcome::Draw,
+            Turn(a, b) if a == b => Draw,
+            Turn(Rock, Paper) => Victory,
+            Turn(Paper, Scissors) => Victory,
+            Turn(Scissors, Rock) => Victory,
+            _ => Defeat,
         }
     }
     pub fn score(&self) -> u64 {
-        (match self {
-            Turn(_, Move::Rock) => 1,
-            Turn(_, Move::Paper) => 2,
-            Turn(_, Move::Scissors) => 3,
-        }) + match self.outcome() {
-            Outcome::Victory => 6,
-            Outcome::Draw => 3,
-            Outcome::Defeat => 0,
-        }
+        self.1 as u64 + self.outcome() as u64
     }
 }
 
@@ -64,9 +56,9 @@ impl From<&str> for Game {
 impl From<&str> for Turn {
     fn from(value: &str) -> Self {
         let mut chars = value.chars();
-        let left = Move::from(&chars.next().expect("unexpected character in input"));
+        let left = Move::from(&chars.next().unwrap());
         chars.next();
-        let right = Move::from(&chars.next().expect("Unexpected character in input"));
+        let right = Move::from(&chars.next().unwrap());
         Turn(left, right)
     }
 }
@@ -74,12 +66,9 @@ impl From<&str> for Turn {
 impl From<&char> for Move {
     fn from(value: &char) -> Self {
         match value {
-            'A' => Self::Rock,
-            'B' => Self::Paper,
-            'C' => Self::Scissors,
-            'X' => Self::Rock,
-            'Y' => Self::Paper,
-            'Z' => Self::Scissors,
+            'A' | 'X' => Self::Rock,
+            'B' | 'Y' => Self::Paper,
+            'C' | 'Z' => Self::Scissors,
             c => panic!("Cannot convert {c} to Move"),
         }
     }
