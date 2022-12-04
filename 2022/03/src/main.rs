c0352f04b55c16896b95ff7e8de9c6dd204329e5
cor <cor@pruijs.dev>
2022-12-04 18:37:32 +0100

    feat: split intersects

--- 2022/03/src/main.rs
+++ 2022/03/src/main.rs
@@ -3,10 +3,12 @@ use std::collections::HashSet;
 
 fn main() {
     let input = challenge_input();
-    let splits = input
+    let splits: Vec<Vec<char>> = input
         .lines()
-        .map(|s| s.split_at(s.len() / 2))
+        .map(|l| l.split_at(l.len() / 2))
         .map(|(a, b)| (HashSet::from_iter(a.chars()), HashSet::from_iter(b.chars())))
-        .collect::<Vec<(HashSet<char>, HashSet<char>)>>();
+        .map(|(a, b): (HashSet<char>, HashSet<char>)| a.intersection(&b).copied().collect())
+        .collect();
+
     dbg!(splits);
 }
