bb2471d78d4d87ba4999ab8b41727cd16fb8d6a5
cor <cor@pruijs.dev>
2022-12-04 21:10:01 +0100

    feat: 2022/03 part 1

--- 2022/03/src/main.rs
+++ 2022/03/src/main.rs
@@ -3,12 +3,15 @@ use std::collections::HashSet;
 
 fn main() {
     let input = challenge_input();
-    let splits: Vec<Vec<char>> = input
+    println!("{}", part_1(&input));
+}
+
+fn part_1(input: &str) -> u32 {
+    input
         .lines()
         .map(|l| l.split_at(l.len() / 2))
         .map(|(a, b)| (HashSet::from_iter(a.chars()), HashSet::from_iter(b.chars())))
-        .map(|(a, b): (HashSet<char>, HashSet<char>)| a.intersection(&b).copied().collect())
-        .collect();
-
-    dbg!(splits);
+        .filter_map(|(a, b): (HashSet<char>, HashSet<char>)| a.intersection(&b).next().copied())
+        .map(|c| ((c as u32 - 'A' as u32) + 27) % 58)
+        .sum()
 }
