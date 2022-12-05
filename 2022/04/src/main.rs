b76f54f37869c1753ef4e420e874a6a99fede20b
cor <cor@pruijs.dev>
2022-12-05 16:48:02 +0100

    feat: 2022/04 part 2

--- 2022/04/src/main.rs
+++ 2022/04/src/main.rs
@@ -1,16 +1,31 @@
 use aoc_2022_common::challenge_input;
 
 fn main() {
-    let input = challenge_input();
-    println!("{}", day_1(&input));
+    let ranges = parse_input(&challenge_input());
+
+    println!("{}", day_1(&ranges));
+    println!("{}", day_2(&ranges));
 }
 
-fn day_1(input: &str) -> usize {
+fn parse_input(input: &str) -> Vec<(Range, Range)> {
     input
         .lines()
         .filter_map(|l| l.split_once(','))
         .map(|(a, b)| (Range::from(a), Range::from(b)))
+        .collect()
+}
+
+fn day_1(ranges: &[(Range, Range)]) -> usize {
+    ranges
+        .iter()
         .filter(|(a, b)| a.contains(b) || b.contains(a))
+        .count()
+}
+
+fn day_2(ranges: &[(Range, Range)]) -> usize {
+    ranges
+        .iter()
+        .filter(|(a, b)| a.overlaps(b) || b.overlaps(a))
         .count()
 }
 
@@ -26,5 +41,9 @@ impl From<&str> for Range {
 impl Range {
     fn contains(&self, other: &Range) -> bool {
         self.0 <= other.0 && self.1 >= other.1
+    }
+
+    fn overlaps(&self, other: &Range) -> bool {
+        (self.0 >= other.0 && self.0 <= other.1) || (self.1 >= other.0 && self.1 <= other.1)
     }
 }
