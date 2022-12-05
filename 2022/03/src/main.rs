64fc62cd3927e04b1e07b0df6844780fb404a412
cor <cor@pruijs.dev>
2022-12-05 13:29:32 +0100

    feat: 2022/03 part 2

--- 2022/03/src/main.rs
+++ 2022/03/src/main.rs
@@ -1,17 +1,48 @@
 use aoc_2022_common::challenge_input;
 use std::collections::HashSet;
 
+type CharSet = HashSet<char>;
+
 fn main() {
     let input = challenge_input();
     println!("{}", part_1(&input));
+    println!("{}", part_2(&input));
 }
 
-fn part_1(input: &str) -> u32 {
+fn part_1(input: &str) -> u64 {
     input
         .lines()
         .map(|l| l.split_at(l.len() / 2))
-        .map(|(a, b)| (HashSet::from_iter(a.chars()), HashSet::from_iter(b.chars())))
-        .filter_map(|(a, b): (HashSet<char>, HashSet<char>)| a.intersection(&b).next().copied())
-        .map(|c| ((c as u32 - 'A' as u32) + 27) % 58)
+        .map(|(a, b)| (to_charset(a), to_charset(b)))
+        .filter_map(|(a, b)| a.intersection(&b).next().copied())
+        .map(char_to_priority)
         .sum()
+}
+
+fn part_2(input: &str) -> u64 {
+    input
+        .lines()
+        .map(to_charset)
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .filter_map(|chunk| match chunk {
+            [a, b, c] => a
+                .intersection(b)
+                .copied()
+                .collect::<CharSet>()
+                .intersection(c)
+                .next()
+                .copied(),
+            _ => panic!("Invalid chunks in input"),
+        })
+        .map(char_to_priority)
+        .sum()
+}
+
+fn char_to_priority(c: char) -> u64 {
+    ((c as u64 - 'A' as u64) + 27) % 58
+}
+
+fn to_charset(s: &str) -> CharSet {
+    CharSet::from_iter(s.chars())
 }
