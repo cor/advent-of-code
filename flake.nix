dc7453897dcc79de52c8d89f35fd0b52e0d9ee05
cor <cor@pruijs.dev>
2022-12-09 14:19:52 +0100

    fix: override for non-existent input 'nixpkgs'

--- flake.nix
+++ flake.nix
@@ -3,10 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
