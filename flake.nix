e31188bd4d1b1961becf36b08789f64d671c2ceb
cor <cor@pruijs.dev>
2022-12-11 16:09:17 +0100

    feat(2022-10): init

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
             lib = self.inputs.crane.lib.${system};
             stable = lib.overrideToolchain self'.packages.rust-stable;
           };
-          days = map (pkgs.lib.fixedWidthNumber 2) (pkgs.lib.range 1 9);
+          days = map (pkgs.lib.fixedWidthNumber 2) (pkgs.lib.range 1 10);
         in {
           packages = {
             rust-stable = inputs'.rust-overlay.packages.rust.override {
