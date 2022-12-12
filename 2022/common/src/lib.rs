0a0046194ea19b1225c19df97ac61e18b05a5966
cor <cor@pruijs.dev>
2022-12-12 01:24:40 +0100

    feat(common): better error messages

--- 2022/common/src/lib.rs
+++ 2022/common/src/lib.rs
@@ -4,6 +4,9 @@ use std::fs;
 #[must_use]
 pub fn challenge_input() -> String {
     let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
-    fs::read_to_string(file_path).expect("Unable to read input file")
+    let file_path = &args
+        .get(1)
+        .expect("⚠️Please pass a path to an input file! ⚠️");
+
+    fs::read_to_string(file_path).expect("⚠️Unable to read input file! ⚠️")
 }
