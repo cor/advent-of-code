f3216c3f248f92bb33811868854f18a4d6b100e9
cor <cor@pruijs.dev>
2022-12-13 00:34:27 +0100

    feat(2022-10: solve part1

--- 2022/10/src/main.rs
+++ 2022/10/src/main.rs
@@ -12,8 +12,32 @@ use nom::{
 
 fn main() {
     let input = challenge_input();
-    let instructions = Instruction::parse_list0(&input);
-    dbg!("{}", instructions);
+    let (_, instructions) = Instruction::parse_list0(&input).expect("Invalid input");
+
+    let mut x_history: Vec<i64> = vec![1, 1];
+
+    for instr in instructions {
+        let last = *x_history.last().unwrap() as i64;
+
+        x_history.push(last);
+        if let Instruction::Addx(n) = instr {
+            x_history.push(last + n);
+        }
+    }
+
+    let signals = x_history
+        .iter()
+        .enumerate()
+        .map(|(i, x)| i as i64 * x)
+        .collect::<Vec<_>>();
+
+    if signals.len() >= 220 {
+        let sum =
+            signals[20] + signals[60] + signals[100] + signals[140] + signals[180] + signals[220];
+        println!("{}", sum);
+    } else {
+        println!("Output is too short to produce signal sum");
+    }
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
