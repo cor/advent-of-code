f3c37618c5bec4e59267d884e2fa347dfb02d859
cor <cor@pruijs.dev>
2022-12-17 15:35:58 +0100

    feat(2022-12): solve part 2

--- 2022/12/src/main.rs
+++ 2022/12/src/main.rs
@@ -13,45 +13,37 @@ const DIRECTIONS: [Point; 4] = [Point(0, 1), Point(0, -1), Point(1, 0), Point(-1
 
 fn main() {
     let input = challenge_input();
-    let (map, _height, width) = parse_input(&input);
+    let map = parse_input(&input);
 
     let start_index = map
         .iter()
         .position(|&c| c == MapItem::Start)
         .expect("No start on map");
     let start = Point(
-        (start_index / width) as isize,
-        (start_index % width) as isize,
+        (start_index / map.nrows()) as isize,
+        (start_index % map.nrows()) as isize,
     );
 
-    let mut steps = Vec::<HashSet<Point>>::new();
-    let mut visited = HashSet::<Point>::new();
-    visited.insert(start);
-    steps.push(visited.clone());
+    println!(
+        "{}",
+        start
+            .steps_to_end(&map)
+            .expect("part 1 should have a solution")
+    );
 
-    for step_number in 1.. {
-        let next_steps: HashSet<Point> = steps
-            .last()
-            .unwrap()
-            .iter()
-            .flat_map(|s| s.neighbors_to_go_to(&map))
-            .filter(|s| !visited.contains(s))
-            .collect();
+    let part_2 = map
+        .iter()
+        .enumerate()
+        .filter(|(_, &item)| item == MapItem::Start || item == MapItem::Level(1))
+        .map(|(i, _)| Point((i / map.nrows()) as isize, (i % map.nrows()) as isize))
+        .filter_map(|p| p.steps_to_end(&map))
+        .min()
+        .unwrap();
 
-        if next_steps
-            .iter()
-            .any(|&s| s.on_map(&map) == Some(MapItem::End))
-        {
-            println!("{}", step_number);
-            break;
-        }
-
-        visited.extend(&next_steps);
-        steps.push(next_steps);
-    }
+    println!("{}", part_2);
 }
 
-fn parse_input(input: &str) -> (DMatrix<MapItem>, usize, usize) {
+fn parse_input(input: &str) -> DMatrix<MapItem> {
     let lines: Vec<&str> = input.lines().collect();
     let rows = lines.len();
     let cols = lines[0].len();
@@ -66,15 +58,44 @@ fn parse_input(input: &str) -> (DMatrix<MapItem>, usize, usize) {
             })
         })
         .collect();
-
-    (
-        DMatrix::from_row_slice(rows, cols, &height_data),
-        rows,
-        cols,
-    )
+    DMatrix::from_row_slice(rows, cols, &height_data)
 }
 
 impl Point {
+    fn steps_to_end(&self, map: &DMatrix<MapItem>) -> Option<usize> {
+        let mut steps = Vec::<HashSet<Point>>::new();
+        let mut visited = HashSet::<Point>::new();
+        visited.insert(*self);
+        steps.push(visited.clone());
+
+        for step_number in 1.. {
+            let next_steps: HashSet<Point> = steps
+                .last()
+                .expect("no last steps")
+                .iter()
+                .flat_map(|s| s.neighbors_to_go_to(map))
+                .filter(|s| !visited.contains(s))
+                .collect();
+
+            if next_steps.is_empty() {
+                // It is impossible to go to the end from this starting position
+                // as we have not reached the end yet, but there are no next_steps
+                return None;
+            }
+
+            if next_steps
+                .iter()
+                .any(|&s| s.on_map(map) == Some(MapItem::End))
+            {
+                return Some(step_number);
+            }
+
+            visited.extend(&next_steps);
+            steps.push(next_steps);
+        }
+        unreachable!();
+    }
+
     pub fn neighbors_to_go_to(&self, map: &DMatrix<MapItem>) -> Vec<Self> {
         let self_item = self.on_map(map).expect("querying neighbors outside of map");
         DIRECTIONS
