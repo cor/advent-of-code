02032d549847e2d150e27a907e56b8bdeebf6d2e
cor <cor@pruijs.dev>
2022-12-17 14:45:09 +0100

    feat(2022-12): calculate valid neighbors to go to

--- 2022/12/src/main.rs
+++ 2022/12/src/main.rs
@@ -2,15 +2,36 @@ use std::fmt::Display;
 
 use aoc_2022_common::challenge_input;
 
+use derive_more::{Add, AddAssign};
 use nalgebra::DMatrix;
+
+// copied form day 8
+#[derive(Debug, Eq, PartialEq, Clone, Copy, Add, AddAssign)]
+struct Point(isize, isize);
+
+const DIRECTIONS: [Point; 4] = [Point(0, 1), Point(0, -1), Point(1, 0), Point(-1, 0)];
 
 fn main() {
     let input = challenge_input();
-    let matrix = parse_input(&input);
-    println!("{}", matrix);
+    let (map, _height, width) = parse_input(&input);
+    println!("{}", map);
+
+    let start_index = map
+        .iter()
+        .position(|&c| c == MapItem::Start)
+        .expect("No start on map");
+    let start = Point(
+        (start_index / width) as isize,
+        (start_index % width) as isize,
+    );
+    dbg!(start);
+    dbg!(start.neighbors_to_go_to(&map));
+    dbg!(Point(2, 3).neighbors_to_go_to(&map));
+    dbg!(Point(4, 1).on_map(&map));
+    dbg!(start_index);
 }
 
-fn parse_input(input: &str) -> DMatrix<MapItem> {
+fn parse_input(input: &str) -> (DMatrix<MapItem>, usize, usize) {
     let lines: Vec<&str> = input.lines().collect();
     let rows = lines.len();
     let cols = lines[0].len();
@@ -26,7 +47,38 @@ fn parse_input(input: &str) -> DMatrix<MapItem> {
         })
         .collect();
 
-    DMatrix::from_row_slice(rows, cols, &height_data)
+    (
+        DMatrix::from_row_slice(rows, cols, &height_data),
+        rows,
+        cols,
+    )
+}
+
+impl Point {
+    pub fn neighbors_to_go_to(&self, map: &DMatrix<MapItem>) -> Vec<Self> {
+        let self_item = self.on_map(map).expect("querying neighbors outside of map");
+        DIRECTIONS
+            .iter()
+            .filter(|&&dir| {
+                (*self + dir)
+                    .on_map(map)
+                    .map(|neighbor_item| self_item.can_move_to(neighbor_item))
+                    .unwrap_or(false)
+            })
+            .map(|&dir| *self + dir)
+            .collect::<Vec<_>>()
+    }
+
+    pub fn on_map<T: Copy>(&self, map: &DMatrix<T>) -> Option<T> {
+        let column: Option<usize> = self.0.try_into().ok();
+        let row: Option<usize> = self.1.try_into().ok();
+        let upoint = (row, column);
+
+        match upoint {
+            (Some(y), Some(x)) => map.get((y, x)).copied(),
+            (None | Some(_), None) | (None, Some(_)) => None,
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -34,6 +86,19 @@ enum MapItem {
     Start,
     End,
     Level(u64),
+}
+
+impl MapItem {
+    pub fn as_level(&self) -> u64 {
+        match self {
+            MapItem::Start => 1,
+            MapItem::End => 26,
+            MapItem::Level(l) => *l,
+        }
+    }
+    pub fn can_move_to(&self, other: MapItem) -> bool {
+        other.as_level() <= self.as_level() + 1
+    }
 }
 
 impl Display for MapItem {
