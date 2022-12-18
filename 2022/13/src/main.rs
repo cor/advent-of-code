9f56ba92babb13e325601b5e4a735df65f7582ce
cor <cor@pruijs.dev>
2022-12-18 23:02:31 +0100

    feat(2022-13): solve part 2

--- 2022/13/src/main.rs
+++ 2022/13/src/main.rs
@@ -6,12 +6,12 @@ use nom::{
     character::complete::u64,
     character::{complete::line_ending, streaming::char},
     combinator::map,
-    multi::{count, separated_list0},
-    sequence::{delimited, separated_pair},
+    multi::{count, many0, separated_list0},
+    sequence::{delimited, preceded, separated_pair},
     IResult,
 };
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 enum Packet {
     List(Vec<Packet>),
     Integer(u64),
@@ -35,6 +35,14 @@ impl Packet {
 
     pub fn parse_pair_list0(input: &str) -> IResult<&str, Vec<(Packet, Packet)>> {
         separated_list0(count(line_ending, 2), Self::parse_pair)(input)
+    }
+
+    pub fn parse_list0(input: &str) -> IResult<&str, Vec<Packet>> {
+        separated_list0(line_ending, preceded(many0(line_ending), Self::parse))(input)
+    }
+
+    pub fn new_divider(number: u64) -> Self {
+        Packet::List(vec![Packet::List(vec![Packet::Integer(number)])])
     }
 }
 
@@ -67,17 +75,38 @@ impl PartialOrd<Self> for Packet {
     }
 }
 
-fn main() {
-    let input = challenge_input();
-    let (_, packets) = Packet::parse_pair_list0(&input).expect("invalid packets in input");
-    let res = &packets
+#[must_use]
+pub fn part_1(input: &str) -> usize {
+    Packet::parse_pair_list0(input)
+        .expect("invalid packets in input")
+        .1
         .iter()
         .map(|(lhs, rhs)| lhs.cmp(rhs))
         .enumerate()
         .map(|(index, ord)| (index + 1, ord))
         .filter(|(_, ord)| *ord != Ordering::Greater)
         .map(|(index, _)| index)
-        .sum::<usize>();
+        .sum::<usize>()
+}
 
-    println!("{}", res);
+#[must_use]
+pub fn part_2(input: &str) -> usize {
+    let (_, mut packets) = Packet::parse_list0(input).expect("invalid packets in input");
+    let divider_packet_2 = Packet::new_divider(2);
+    let divider_packet_6 = Packet::new_divider(6);
+    packets.push(divider_packet_2.clone());
+    packets.push(divider_packet_6.clone());
+    packets.sort_unstable();
+
+    let divider_2_index = packets.iter().position(|p| *p == divider_packet_2).unwrap() + 1;
+    let divider_6_index = packets.iter().position(|p| *p == divider_packet_6).unwrap() + 1;
+
+    divider_2_index * divider_6_index
+}
+
+fn main() {
+    let input = challenge_input();
+
+    println!("{}", part_1(&input));
+    println!("{}", part_2(&input));
 }
