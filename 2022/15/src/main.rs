3f77533c3c48fbbb2f8b5d992a3fac356b1a59d4
cor <cor@pruijs.dev>
2022-12-19 12:30:10 +0100

    feat(2022-15): parse input

--- 2022/15/src/main.rs
+++ 2022/15/src/main.rs
@@ -1,6 +1,61 @@
 use aoc_2022_common::challenge_input;
 
+use derive_more::{Add, Constructor};
+
+use nom::sequence::preceded;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{i32, line_ending},
+    combinator::map,
+    multi::separated_list0,
+    sequence::separated_pair,
+    IResult,
+};
+
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug, Add, Constructor)]
+struct Point {
+    pub x: i32,
+    pub y: i32,
+}
+
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug, Add, Constructor)]
+struct Sensor {
+    pub position: Point,
+    pub closest_beacon: Point,
+}
+
+impl Sensor {
+    pub fn parse(input: &str) -> IResult<&str, Self> {
+        map(
+            preceded(
+                tag("Sensor at "),
+                separated_pair(Point::parse, tag(": closest beacon is at "), Point::parse),
+            ),
+            |(position, closest_beacon)| Sensor::new(position, closest_beacon),
+        )(input)
+    }
+
+    pub fn parse_list0(input: &str) -> IResult<&str, Vec<Self>> {
+        separated_list0(line_ending, Self::parse)(input)
+    }
+}
+
+impl Point {
+    pub fn parse(input: &str) -> IResult<&str, Self> {
+        map(
+            separated_pair(
+                preceded(tag("x="), i32),
+                tag(", "),
+                preceded(tag("y="), i32),
+            ),
+            |(x, y)| Self::new(x, y),
+        )(input)
+    }
+}
+
 fn main() {
     let input = challenge_input();
+    let (_, sensors) = Sensor::parse_list0(&input).expect("Invalid sensors in input");
     println!("{}", input);
+    dbg!(sensors);
 }
