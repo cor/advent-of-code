2cb30c46afc0b86bf0ff72c044f792e303e7861e
cor <cor@pruijs.dev>
2022-12-19 19:06:50 +0100

    fix: update deps and inherit inputs

--- flake.nix
+++ flake.nix
@@ -13,8 +13,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, rust-overlay, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit self; } {
+  outputs = inputs@{ self, nixpkgs, rust-overlay, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems =
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
