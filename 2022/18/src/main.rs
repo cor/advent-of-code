6e76ad2644c09ef1a29f569c61fc7f6bea140feb
cor <cor@pruijs.dev>
2022-12-22 15:52:42 +0100

    feat(2022-18): part 2 wippp

--- 2022/18/src/main.rs
+++ 2022/18/src/main.rs
@@ -41,6 +41,39 @@ impl Point {
             .filter(|&side| !others.contains(&(*self + *side)))
             .count()
     }
+    pub fn really_exposed_sides(&self, others: &HashSet<Self>) -> usize {
+        SIDES
+            .iter()
+            .filter(|&side| {
+                let start = *self + *side;
+                if others.contains(&start) {
+                    return false;
+                }
+                let mut visited: HashSet<Self> = HashSet::new();
+                let mut steps = Vec::<HashSet<Point>>::new();
+                visited.insert(start);
+                steps.push(visited.clone());
+
+                for _ in 1..20 {
+                    let last = steps.last().unwrap();
+                    if last.is_empty() {
+                        return false;
+                    }
+
+                    let next = last
+                        .iter()
+                        .flat_map(|last| SIDES.iter().map(|&s| *last + s))
+                        .filter(|p| !visited.contains(p) && !others.contains(p))
+                        .collect::<HashSet<Point>>();
+
+                    visited.extend(&next);
+                    steps.push(next);
+                }
+
+                true
+            })
+            .count()
+    }
 }
 
 fn main() {
@@ -48,6 +81,8 @@ fn main() {
     let points = Point::parse_list0(&input);
     // let points = Point::parse_list0("1,1,1\n2,1,1");
     let exposed_sides: usize = points.iter().map(|p| p.exposed_sides(&points)).sum();
+    let really_exposed_sides: usize = points.iter().map(|p| p.really_exposed_sides(&points)).sum();
 
     println!("{exposed_sides}");
+    println!("{really_exposed_sides}");
 }
