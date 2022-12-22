d93bab14a97d8963e3d977efb94dced52e6ae4fa
cor <cor@pruijs.dev>
2022-12-22 14:01:47 +0100

    feat(2022-18): solve part 1

--- 2022/18/src/main.rs
+++ 2022/18/src/main.rs
@@ -1,6 +1,18 @@
-use aoc_2022_common::challenge_input;
+use std::collections::HashSet;
 
-#[derive(Debug)]
+use aoc_2022_common::challenge_input;
+use derive_more::{Add, Sub};
+
+const SIDES: [Point; 6] = [
+    Point { x: 1, y: 0, z: 0 },
+    Point { x: -1, y: 0, z: 0 },
+    Point { x: 0, y: 1, z: 0 },
+    Point { x: 0, y: -1, z: 0 },
+    Point { x: 0, y: 0, z: 1 },
+    Point { x: 0, y: 0, z: -1 },
+];
+
+#[derive(Debug, Hash, Add, Sub, Eq, PartialEq, Clone, Copy)]
 struct Point {
     x: i64,
     y: i64,
@@ -19,13 +31,23 @@ impl Point {
         }
     }
 
-    pub fn parse_list0(input: &str) -> Vec<Self> {
+    pub fn parse_list0(input: &str) -> HashSet<Self> {
         input.lines().map(Self::parse).collect()
+    }
+
+    pub fn exposed_sides(&self, others: &HashSet<Self>) -> usize {
+        SIDES
+            .iter()
+            .filter(|&side| !others.contains(&(*self + *side)))
+            .count()
     }
 }
 
 fn main() {
     let input = challenge_input();
     let points = Point::parse_list0(&input);
-    dbg!(points);
+    // let points = Point::parse_list0("1,1,1\n2,1,1");
+    let exposed_sides: usize = points.iter().map(|p| p.exposed_sides(&points)).sum();
+
+    println!("{exposed_sides}");
 }
