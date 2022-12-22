bcf90a43fe2a8ac3373fd85d96120d4f558bcaea
cor <cor@pruijs.dev>
2022-12-22 11:37:10 +0100

    feat(2022-20): solve part 1

---
+++ 2022/20/src/main.rs
@@ -0,0 +1,43 @@
+use aoc_2022_common::challenge_input;
+
+#[derive(Debug, Clone)]
+struct Number {
+    value: i64,
+    index: usize,
+}
+
+fn main() {
+    let input = challenge_input();
+    let input_numbers: Vec<Number> = input
+        .lines()
+        .enumerate()
+        .map(|(index, val)| Number {
+            value: val.parse().unwrap(),
+            index,
+        })
+        .collect();
+
+    let len = input_numbers.len();
+    let len_when_moving = (len - 1) as i64;
+
+    let mut moving_numbers = input_numbers.clone();
+
+    for num in &input_numbers {
+        let current_index = moving_numbers
+            .iter()
+            .position(|n| n.index == num.index)
+            .unwrap() as i64;
+
+        let new_index =
+            ((current_index + num.value) % len_when_moving + len_when_moving) % len_when_moving;
+
+        let removed = moving_numbers.remove(current_index as usize);
+        moving_numbers.insert(new_index as usize, removed);
+    }
+
+    let zero_index = moving_numbers.iter().position(|n| n.value == 0).unwrap();
+    let sum = moving_numbers[(zero_index + 1000) % len].value
+        + moving_numbers[(zero_index + 2000) % len].value
+        + moving_numbers[(zero_index + 3000) % len].value;
+    println!("{sum}");
+}
