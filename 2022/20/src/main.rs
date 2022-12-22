0b9a3fcadbd9bcdf5257371ce63684983d082583
cor <cor@pruijs.dev>
2022-12-22 11:55:22 +0100

    refactor(2022-20): mix function

--- 2022/20/src/main.rs
+++ 2022/20/src/main.rs
@@ -6,23 +6,10 @@ struct Number {
     index: usize,
 }
 
-fn main() {
-    let input = challenge_input();
-    let input_numbers: Vec<Number> = input
-        .lines()
-        .enumerate()
-        .map(|(index, val)| Number {
-            value: val.parse().unwrap(),
-            index,
-        })
-        .collect();
+fn mix(input_numbers: &[Number], moving_numbers: &mut Vec<Number>) {
+    let len_when_moving = (input_numbers.len() - 1) as i64;
 
-    let len = input_numbers.len();
-    let len_when_moving = (len - 1) as i64;
-
-    let mut moving_numbers = input_numbers.clone();
-
-    for num in &input_numbers {
+    for num in input_numbers {
         let current_index = moving_numbers
             .iter()
             .position(|n| n.index == num.index)
@@ -34,7 +21,24 @@ fn main() {
         let removed = moving_numbers.remove(current_index as usize);
         moving_numbers.insert(new_index as usize, removed);
     }
+}
 
+fn main() {
+    let input = challenge_input();
+    let input_numbers: Vec<Number> = input
+        .lines()
+        .enumerate()
+        .map(|(index, val)| Number {
+            value: val.parse().unwrap(),
+            index,
+        })
+        .collect();
+
+    let mut moving_numbers = input_numbers.clone();
+
+    mix(&input_numbers, &mut moving_numbers);
+
+    let len = &input_numbers.len();
     let zero_index = moving_numbers.iter().position(|n| n.value == 0).unwrap();
     let sum = moving_numbers[(zero_index + 1000) % len].value
         + moving_numbers[(zero_index + 2000) % len].value
