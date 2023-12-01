8a76aa42bfb62502ca5058ca13b5453529de5587
cor <cor@pruijs.dev>
2023-12-01 11:18:52 +0100

    feat(2023-01): solve part 1

--- 2023/01/src/main.rs
+++ 2023/01/src/main.rs
@@ -1,6 +1,21 @@
 use aoc_2023_common::challenge_input;
 
+fn first_digit(line: &str) -> Option<char> {
+    line.chars().find(|c| c.is_numeric())
+}
+
+fn last_digit(line: &str) -> Option<char> {
+    line.chars().rev().find(|c| c.is_numeric())
+}
+
+fn calibration_value(line: &str) -> Option<usize> {
+    let first = first_digit(line)?;
+    let last = last_digit(line)?;
+    [first, last].iter().collect::<String>().parse().ok()
+}
+
 fn main() {
     let input = challenge_input();
-    println!("{input}");
+    let part_1: usize = input.lines().filter_map(calibration_value).sum();
+    println!("{part_1}");
 }
