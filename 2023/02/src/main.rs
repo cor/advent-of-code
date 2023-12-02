c13bc2f6f8ba297187ef5a84cdc49cb4aa5a0049
cor <cor@pruijs.dev>
2023-12-02 20:23:48 +0100

    feat(2023-02): solve part 1

---
+++ 2023/02/src/main.rs
@@ -0,0 +1,68 @@
+use aoc_2023_common::challenge_input;
+use nom::{
+    bytes::complete::tag,
+    character::{
+        complete::{alphanumeric1, space1, u32},
+        streaming::newline,
+    },
+    combinator::map,
+    multi::separated_list0,
+    sequence::preceded,
+    sequence::separated_pair,
+    IResult,
+};
+
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    sets: Vec<(u32, u32, u32)>,
+}
+
+impl Game {
+    pub fn parse_many(input: &str) -> IResult<&str, Vec<Self>> {
+        separated_list0(newline, Self::parse)(input)
+    }
+    pub fn parse(input: &str) -> IResult<&str, Self> {
+        map(
+            separated_pair(
+                preceded(tag("Game "), u32),
+                tag(": "),
+                separated_list0(
+                    tag("; "),
+                    separated_list0(tag(", "), separated_pair(u32, space1, alphanumeric1)),
+                ),
+            ),
+            |(id, sets)| {
+                let sets = sets
+                    .iter()
+                    .map(|set| {
+                        set.iter()
+                            .fold((0, 0, 0), |(r, g, b), (count, color)| match *color {
+                                "red" => (r + count, g, b),
+                                "green" => (r, g + count, b),
+                                "blue" => (r, g, b + count),
+                                _ => (r, g, b),
+                            })
+                    })
+                    .collect();
+                Game { id, sets }
+            },
+        )(input)
+    }
+}
+
+fn main() {
+    let input = challenge_input();
+    let games = Game::parse_many(&input).expect("invalid input").1;
+    let part_1: u32 = games
+        .iter()
+        .filter(|game| {
+            game.sets
+                .iter()
+                .all(|(r, g, b)| *r <= 12 && *g <= 13 && *b <= 14)
+        })
+        .map(|game| game.id)
+        .sum();
+    // dbg!(games);
+    println!("{part_1}");
+}
