7015d31784fef15872f6c53fbf5b7eb58dcf5d1f
cor <cor@pruijs.dev>
2023-12-03 15:33:18 +0100

    feat(2023-03): solve part 2

--- 2023/03/src/main.rs
+++ 2023/03/src/main.rs
@@ -1,23 +1,22 @@
+use std::collections::HashMap;
+
 use aoc_2023_common::challenge_input;
-
-// fn print_schematic(schematic: Vec<Vec<char>>) {
-//     for line in schematic {
-//         for char in line {
-//             print!("{char}");
-//         }
-//         println!();
-//     }
-// }
-
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 struct PartNumber {
     number: usize,
     x: (usize, usize),
     y: usize,
 }
 
+#[derive(Debug, PartialEq, Eq)]
+enum PartType {
+    Invalid,
+    Part,
+    Gear(usize, usize),
+}
+
 impl PartNumber {
-    fn is_valid(&self, schematic: &[Vec<char>], width: i32, height: i32) -> bool {
+    fn is_valid(&self, schematic: &[Vec<char>], width: i32, height: i32) -> PartType {
         for x in self.x.0..=self.x.1 {
             for (dx, dy) in NEIGHBORS {
                 let new_x = x as i32 + dx;
@@ -26,12 +25,13 @@ impl PartNumber {
                     let z = schematic[new_y as usize][new_x as usize];
                     match z {
                         '0'..='9' | '.' => {}
-                        _ => return true,
+                        '*' => return PartType::Gear(new_x as usize, new_y as usize),
+                        _ => return PartType::Part,
                     }
                 }
             }
         }
-        false
+        PartType::Invalid
     }
 }
 
@@ -78,11 +78,33 @@ fn main() {
     let part_numbers = find_part_numbers(&schematic);
     let height = schematic.len() as i32;
     let width = schematic.first().unwrap().len() as i32;
-
     let part_1: usize = part_numbers
         .iter()
-        .filter(|part_number| part_number.is_valid(&schematic, width, height))
+        .filter(|part_number| {
+            !matches!(
+                part_number.is_valid(&schematic, width, height),
+                PartType::Invalid
+            )
+        })
         .map(|part| part.number)
         .sum();
+
     println!("{part_1}");
+
+    let mut gears = HashMap::new();
+    for part in part_numbers {
+        if let PartType::Gear(x, y) = part.is_valid(&schematic, width, height) {
+            let point = (x, y);
+            let entry = gears.entry(point).or_insert(Vec::new());
+            entry.push(part);
+        };
+    }
+
+    let part_2: usize = gears
+        .iter()
+        .filter(|(_, gears)| gears.len() == 2)
+        .map(|(_, gears)| gears.iter().map(|g| g.number).product::<usize>())
+        .sum();
+
+    println!("{part_2}");
 }
