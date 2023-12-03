f70fadc077c427a4678897a1dcae6b4c037a5bb6
cor <cor@pruijs.dev>
2023-12-03 12:27:31 +0100

    feat(2023-01): solve part 1

---
+++ 2023/03/src/main.rs
@@ -0,0 +1,84 @@
+use aoc_2023_common::challenge_input;
+
+// fn print_schematic(schematic: Vec<Vec<char>>) {
+//     for line in schematic {
+//         for char in line {
+//             print!("{char}");
+//         }
+//         println!();
+//     }
+// }
+
+#[derive(Debug)]
+struct PartNumber {
+    number: usize,
+    x: (usize, usize),
+    y: usize,
+}
+
+fn find_part_numbers(schematic: &[Vec<char>]) -> Vec<PartNumber> {
+    let mut part_numbers: Vec<PartNumber> = Vec::new();
+
+    for (y, line) in schematic.iter().enumerate() {
+        let mut char_iter = line.iter().enumerate();
+        while let Some((x0, &char)) = char_iter.next() {
+            if char.is_ascii_digit() {
+                let mut number_digits = vec![char];
+                let mut x1 = x0;
+                while let Some((char_index_inner, &char_inner @ '0'..='9')) = char_iter.next() {
+                    number_digits.push(char_inner);
+                    x1 = char_index_inner;
+                }
+                let number = number_digits.iter().collect::<String>().parse().unwrap();
+                part_numbers.push(PartNumber {
+                    number,
+                    x: (x0, x1),
+                    y,
+                });
+            }
+        }
+        println!();
+    }
+    part_numbers
+}
+
+const NEIGHBORS: [(i32, i32); 8] = [
+    (0, -1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+    (0, 1),
+    (-1, 1),
+    (-1, 0),
+    (-1, -1),
+];
+
+fn main() {
+    let input = challenge_input();
+    let schematic: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+    let part_numbers = find_part_numbers(&schematic);
+    let height = schematic.len() as i32;
+    let width = schematic.first().unwrap().len() as i32;
+
+    let part_1: usize = part_numbers
+        .iter()
+        .filter(|part_number| {
+            for x in part_number.x.0..=part_number.x.1 {
+                for (dx, dy) in NEIGHBORS {
+                    let new_x = x as i32 + dx;
+                    let new_y = part_number.y as i32 + dy;
+                    if new_x >= 0 && new_x < width && new_y >= 0 && new_y < height {
+                        let z = schematic[new_y as usize][new_x as usize];
+                        match z {
+                            '0'..='9' | '.' => {}
+                            _ => return true,
+                        }
+                    }
+                }
+            }
+            false
+        })
+        .map(|part| part.number)
+        .sum();
+    println!("{part_1}");
+}
