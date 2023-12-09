d8e882eee8f44db8e3d145a9dbcf9ff14cc0ee6c
cor <cor@pruijs.dev>
2023-12-09 02:20:40 +0100

    feat(2023-08): solve part 1

--- 2023/08/src/main.rs
+++ 2023/08/src/main.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use aoc_2023_common::challenge_input;
 
 type Node = [char; 3];
+const TARGET_NODE: Node = ['Z', 'Z', 'Z'];
 
 trait Parse {
     fn parse(input: &str) -> Self;
@@ -33,10 +34,7 @@ struct Map {
 impl Map {
     fn parse(input: &str) -> Self {
         let (instructions, network) = input.split_once("\n\n").expect("Invalid input");
-        let instructions = instructions
-            .chars()
-            .map(Instruction::parse)
-            .collect::<Vec<_>>();
+        let instructions = instructions.chars().map(Instruction::parse).collect();
         let network = network
             .lines()
             .map(|line| {
@@ -55,6 +53,52 @@ impl Map {
             network,
         }
     }
+
+    fn step_count(&self) -> usize {
+        let mut node: Node = ['A', 'A', 'A'];
+
+        for step_count in 0.. {
+            let instruction = &self.instructions[step_count % self.instructions.len()];
+            let (left, right) = self.network.get(&node).expect("invalid node in network");
+            match instruction {
+                Instruction::Left => node = *left,
+                Instruction::Right => node = *right,
+            }
+            if node == TARGET_NODE {
+                return step_count + 1;
+            }
+        }
+
+        panic!("QED");
+    }
+
+    fn step_count_2(&self) -> usize {
+        let mut start_nodes: Vec<&Node> = self
+            .network
+            .keys()
+            .filter(|key| matches!(key, [_, _, 'A']))
+            .collect();
+
+        for step_count in 0.. {
+            let instruction = &self.instructions[step_count % self.instructions.len()];
+
+            start_nodes = start_nodes
+                .iter()
+                .map(|&&node| {
+                    let (left, right) = self.network.get(&node).expect("invalid node in network");
+                    match instruction {
+                        Instruction::Left => left,
+                        Instruction::Right => right,
+                    }
+                })
+                .collect();
+            if start_nodes.iter().all(|node| matches!(node, [_, _, 'Z'])) {
+                return step_count + 1;
+            }
+        }
+
+        panic!("QED");
+    }
 }
 
 impl Instruction {
@@ -62,7 +106,7 @@ impl Instruction {
         match input {
             'L' => Self::Left,
             'R' => Self::Right,
-            _ => panic!("Invalid input"),
+            _ => panic!("invalid input"),
         }
     }
 }
@@ -70,6 +114,6 @@ impl Instruction {
 fn main() {
     let input = challenge_input();
     let map = Map::parse(&input);
-    dbg!(map);
-    // println!("{input}");
+    println!("{}", map.step_count());
+    println!("{}", map.step_count_2());
 }
