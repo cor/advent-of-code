93212fa0e831b0dc963bdaba57e7c6a3abb0e116
cor <cor@pruijs.dev>
2023-12-10 22:24:51 +0100

    feat(2023-10): solve part 1

--- 2023/10/src/main.rs
+++ 2023/10/src/main.rs
@@ -1,13 +1,16 @@
-use std::fmt::Display;
+use std::{fmt::Display, ops::Add};
 
 use aoc_2023_common::challenge_input;
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 enum Tile {
     Start,
     Ground,
-    Pipe(Pipe),
+    Pipe(PipeType),
 }
-enum Pipe {
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum PipeType {
     NorthSouth,
     EastWest,
     NorthEast,
@@ -15,11 +18,76 @@ enum Pipe {
     SouthWest,
     SouthEast,
 }
-use Pipe::*;
+
+impl PipeType {
+    fn directions(&self) -> (Direction, Direction) {
+        match self {
+            NorthSouth => (North, South),
+            EastWest => (East, West),
+            NorthEast => (North, East),
+            NorthWest => (North, West),
+            SouthWest => (South, West),
+            SouthEast => (South, East),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+impl Direction {
+    fn inverse(&self) -> Self {
+        match &self {
+            North => South,
+            East => West,
+            South => North,
+            West => East,
+        }
+    }
+}
+
+use Direction::*;
+use PipeType::*;
 use Tile::*;
 
 struct Map {
     tiles: Vec<Vec<Tile>>,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+struct Point {
+    x: i16,
+    y: i16,
+}
+
+impl Add<Direction> for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Direction) -> Self::Output {
+        match rhs {
+            Direction::North => Point {
+                x: self.x,
+                y: self.y - 1,
+            },
+            Direction::East => Point {
+                x: self.x + 1,
+                y: self.y,
+            },
+            Direction::South => Point {
+                x: self.x,
+                y: self.y + 1,
+            },
+            Direction::West => Point {
+                x: self.x - 1,
+                y: self.y,
+            },
+        }
+    }
 }
 
 impl Map {
@@ -43,6 +111,35 @@ impl Map {
             .collect();
 
         Self { tiles }
+    }
+
+    fn get(&self, &Point { x, y }: &Point) -> Tile {
+        if x < 0 || y < 0 {
+            return Ground;
+        }
+
+        match self
+            .tiles
+            .get(y as usize)
+            .and_then(|row| row.get(x as usize))
+        {
+            Some(t) => *t,
+            None => Ground,
+        }
+    }
+
+    fn start_point(&self) -> Point {
+        for (y, row) in self.tiles.iter().enumerate() {
+            for (x, tile) in row.iter().enumerate() {
+                if tile == &Start {
+                    return Point {
+                        x: x as i16,
+                        y: y as i16,
+                    };
+                }
+            }
+        }
+        panic!("no start point");
     }
 }
 
@@ -71,5 +168,24 @@ impl Display for Map {
 fn main() {
     let input = challenge_input();
     let map = Map::parse(&input);
-    println!("{}", map);
+    let start = map.start_point();
+    let mut current_point = start;
+    let mut current_dir = West;
+
+    for i in 0.. {
+        current_point = current_point + current_dir;
+        let tile = map.get(&current_point);
+
+        if let Pipe(pt) = tile {
+            let (dir1, dir2) = pt.directions();
+            current_dir = if current_dir.inverse() == dir1 {
+                dir2
+            } else {
+                dir1
+            };
+        } else {
+            println!("{}", (i + 1) / 2);
+            break;
+        }
+    }
 }
