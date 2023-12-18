1c27f6ac20c548b92575e058fb2ff0c2b4a98629
cor <cor@pruijs.dev>
2023-12-18 01:11:09 +0100

    feat(2023-11): solve part 1

--- 2023/11/src/main.rs
+++ 2023/11/src/main.rs
@@ -1,9 +1,10 @@
-use std::fmt::Display;
+use std::{collections::HashSet, fmt::Display};
 
 use aoc_2023_common::challenge_input;
 
 type Universe = Vec<Vec<bool>>;
 struct ExpandedUniverse(Universe);
+type Galaxies = HashSet<(usize, usize)>;
 
 impl From<Universe> for ExpandedUniverse {
     fn from(original: Universe) -> Self {
@@ -33,6 +34,20 @@ impl From<Universe> for ExpandedUniverse {
     }
 }
 
+impl From<ExpandedUniverse> for Galaxies {
+    fn from(universe: ExpandedUniverse) -> Self {
+        let mut galaxies = HashSet::new();
+        for (y, row) in universe.0.iter().enumerate() {
+            for x in 0..row.len() {
+                if universe.0[y][x] {
+                    galaxies.insert((x, y));
+                }
+            }
+        }
+        galaxies
+    }
+}
+
 impl Display for ExpandedUniverse {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for row in &self.0 {
@@ -48,6 +63,10 @@ impl Display for ExpandedUniverse {
     }
 }
 
+fn manhattan_distance(a: &(usize, usize), b: &(usize, usize)) -> usize {
+    a.0.abs_diff(b.0) + a.1.abs_diff(b.1)
+}
+
 fn parse(input: &str) -> Universe {
     input
         .lines()
@@ -55,10 +74,25 @@ fn parse(input: &str) -> Universe {
         .collect()
 }
 
+fn distances(galaxies: Galaxies) -> usize {
+    galaxies
+        .iter()
+        .flat_map(|galaxy_a| {
+            galaxies
+                .iter()
+                .map(|galaxy_b| manhattan_distance(galaxy_a, galaxy_b))
+                .collect::<Vec<_>>()
+        })
+        .sum::<usize>()
+        / 2
+}
+
 fn main() {
     let input = challenge_input();
     let universe = parse(&input);
     let expanded: ExpandedUniverse = universe.into();
-    println!("{}", expanded);
-    println!("{input}");
+    let galaxies: Galaxies = expanded.into();
+    let part_1 = distances(galaxies);
+
+    println!("{part_1}");
 }
