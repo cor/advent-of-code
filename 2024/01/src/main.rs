911570441a93baf236b7db7cfebe9c94784504ca
cor <cor@pruijs.dev>
2024-12-01 19:27:43 +0000

    feat(2024-01): solve part 1 & 2

--- 2024/01/src/main.rs
+++ 2024/01/src/main.rs
@@ -2,5 +2,30 @@ use aoc_2024_common::challenge_input;
 
 fn main() {
     let input = challenge_input();
-    // println!("hi");
+    let (mut left, mut right): (Vec<isize>, Vec<isize>) = input
+        .lines()
+        .map(|l| {
+            let mut iter = l.split_whitespace();
+            (
+                iter.next().unwrap().parse::<isize>().unwrap(),
+                iter.next().unwrap().parse::<isize>().unwrap(),
+            )
+        })
+        .unzip();
+    left.sort();
+    right.sort();
+
+    let part_1: usize = left
+        .iter()
+        .zip(right.iter())
+        .map(|(&x, &y)| x.abs_diff(y))
+        .sum();
+
+    let part_2: isize = left
+        .iter()
+        .map(|i| i * right.iter().filter(|j| &i == j).count() as isize)
+        .sum();
+
+    println!("{part_1}");
+    println!("{part_2}");
 }
