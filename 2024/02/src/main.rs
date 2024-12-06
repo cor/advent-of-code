3639b01120a97028d1100285e9743be7f5de06b6
cor <cor@pruijs.dev>
2024-12-06 16:05:58 +0000

    feat(2024-02): part 2

--- 2024/02/src/main.rs
+++ 2024/02/src/main.rs
@@ -1,6 +1,18 @@
 use aoc_2024_common::challenge_input;
 use std::cmp::Ordering::*;
 
+fn is_safe(report: &&Vec<isize>) -> bool {
+    match report[0].cmp(&report[1]) {
+        Less => report.iter().skip(1).try_fold(report[0], |acc, &n| {
+            (1..=3).contains(&(n - acc)).then_some(n)
+        }),
+        Equal => None,
+        Greater => report.iter().skip(1).try_fold(report[0], |acc, &n| {
+            (1..=3).contains(&-(n - acc)).then_some(n)
+        }),
+    }
+    .is_some()
+}
 fn main() {
     let input = challenge_input();
 
@@ -13,17 +25,19 @@ fn main() {
         })
         .collect();
 
-    let part_1 = parsed
+    let part_1 = parsed.iter().filter(is_safe).count();
+
+    let part_2 = parsed
         .iter()
-        .filter_map(|report| match report[0].cmp(&report[1]) {
-            Less => report.iter().skip(1).try_fold(report[0], |acc, &n| {
-                (1..=3).contains(&(n - acc)).then_some(n)
-            }),
-            Equal => None,
-            Greater => report.iter().skip(1).try_fold(report[0], |acc, &n| {
-                (1..=3).contains(&-(n - acc)).then_some(n)
-            }),
+        .filter(|&report| {
+            (0..report.len()).any(|to_skip| {
+                let mut report = report.clone();
+                report.remove(to_skip);
+                is_safe(&&report)
+            })
         })
         .count();
+
     println!("{part_1}");
+    println!("{part_2}");
 }
