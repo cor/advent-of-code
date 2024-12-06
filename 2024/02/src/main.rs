93d45198758c876a8f063c8383faa574ad4db556
cor <cor@pruijs.dev>
2024-12-06 14:58:35 +0000

    feat(2024-02): part 1

--- 2024/02/src/main.rs
+++ 2024/02/src/main.rs
@@ -1,12 +1,29 @@
 use aoc_2024_common::challenge_input;
+use std::cmp::Ordering::*;
 
 fn main() {
     let input = challenge_input();
-    println!("{input}");
-    
-    let parsed:  Vec<Vec<usize>> = input.lines().map(|l| l.split_whitespace().map(|n| n.parse::<usize>().unwrap()).collect()).collect();
-    dbg!(parsed);
 
-    // let parsed: Vec<Vec<usize> = input.lines()
-    // .map(|l| l.split_whitespace().map(|n| n.parse()).collect()).collect();
+    let parsed: Vec<Vec<isize>> = input
+        .lines()
+        .map(|l| {
+            l.split_whitespace()
+                .map(|n| n.parse::<isize>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let part_1 = parsed
+        .iter()
+        .filter_map(|report| match report[0].cmp(&report[1]) {
+            Less => report.iter().skip(1).try_fold(report[0], |acc, &n| {
+                (1..=3).contains(&(n - acc)).then_some(n)
+            }),
+            Equal => None,
+            Greater => report.iter().skip(1).try_fold(report[0], |acc, &n| {
+                (1..=3).contains(&-(n - acc)).then_some(n)
+            }),
+        })
+        .count();
+    println!("{part_1}");
 }
