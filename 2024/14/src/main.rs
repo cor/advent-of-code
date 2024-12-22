d01a94c5a2149c26ebea3ef27d9bdd2715513e54
cor <cor@pruijs.dev>
2024-12-22 01:01:07 +0000

    fix(2024-14): solve 1

--- 2024/14/src/main.rs
+++ 2024/14/src/main.rs
@@ -1,23 +1,24 @@
 use aoc_2024_common::challenge_input;
 use nalgebra::Vector2;
+use std::cmp::Ordering::*;
 
-const W: i16 = 11;
-const H: i16 = 7;
+const W: i64 = 101;
+const H: i64 = 103;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 struct Robot {
-    position: Vector2<i16>,
-    velocity: Vector2<i16>,
+    position: Vector2<i64>,
+    velocity: Vector2<i64>,
 }
 
 impl Robot {
-    pub fn position_at(&self, second: i16) -> Vector2<i16> {
+    pub fn position_at(&self, second: i64) -> Vector2<i64> {
         let linear = self.position + (self.velocity * second);
         Vector2::new(linear.x.rem_euclid(W), linear.y.rem_euclid(H))
     }
 }
 
-fn draw_world(robots: &[Robot], second: i16) {
+fn draw_world(robots: &[Robot], second: i64) {
     for y in 0..H {
         for x in 0..W {
             let pos = Vector2::new(x, y);
@@ -33,6 +34,27 @@ fn draw_world(robots: &[Robot], second: i16) {
         println!();
     }
     println!("s = {second}");
+}
+
+fn part_1(robots: &[Robot]) -> i64 {
+    let mut q1 = 0;
+    let mut q2 = 0;
+    let mut q3 = 0;
+    let mut q4 = 0;
+
+    for robot in robots {
+        let pos = robot.position_at(100);
+
+        match (pos.x.cmp(&(W / 2)), pos.y.cmp(&(H / 2))) {
+            (Equal, _) | (_, Equal) => (),
+            (Less, Less) => q1 += 1,
+            (Less, Greater) => q2 += 1,
+            (Greater, Less) => q3 += 1,
+            (Greater, Greater) => q4 += 1,
+        }
+    }
+
+    q1 * q2 * q3 * q4
 }
 
 fn main() {
@@ -52,6 +74,5 @@ fn main() {
         .collect::<Vec<_>>();
 
     draw_world(&robots, 100);
-
-    // dbg!(robots);
+    println!("{}", part_1(&robots));
 }
