ab9d9f1ec4cdd8bf908458de673d49f2b675ab0c
cor <cor@pruijs.dev>
2024-12-22 02:19:43 +0000

    feat(2024-14): solve 2

--- 2024/14/src/main.rs
+++ 2024/14/src/main.rs
@@ -20,6 +20,61 @@ impl Robot {
     }
 }
 
+fn part_1(robots: &[Robot], seconds: i64) -> i64 {
+    let (mut q1, mut q2, mut q3, mut q4) = (0, 0, 0, 0);
+
+    for robot in robots {
+        let pos = robot.position_at(seconds);
+
+        match (pos.x.cmp(&(W / 2)), pos.y.cmp(&(H / 2))) {
+            (Equal, _) | (_, Equal) => (),
+            (Less, Less) => q1 += 1,
+            (Less, Greater) => q2 += 1,
+            (Greater, Less) => q3 += 1,
+            (Greater, Greater) => q4 += 1,
+        }
+    }
+    q1 * q2 * q3 * q4
+}
+
+/// Look for a straight line of at least 10 robots
+fn part_2(robots: &[Robot]) -> i64 {
+    (0..i64::MAX)
+        .map(|second| {
+            robots
+                .iter()
+                .map(|r| r.position_at(second))
+                .collect::<Vec<_>>()
+        })
+        .position(|robots_at_second| {
+            robots_at_second.iter().any(|robot| {
+                (0..10).all(|line_x| {
+                    robots_at_second.contains(&Vector2::new(robot.x + line_x, robot.y))
+                })
+            })
+        })
+        .unwrap() as i64
+}
+
+fn main() {
+    let input = challenge_input();
+    let robots = input
+        .lines()
+        .map(|l| {
+            let (p, v) = l.split_once(' ').unwrap();
+            let (px, py) = p[2..].split_once(',').unwrap();
+            let (vx, vy) = v[2..].split_once(',').unwrap();
+            Robot {
+                position: Vector2::new(px.parse().unwrap(), py.parse().unwrap()),
+                velocity: Vector2::new(vx.parse().unwrap(), vy.parse().unwrap()),
+            }
+        })
+        .collect::<Vec<_>>();
+
+    println!("{}", part_1(&robots, 100));
+    draw_world(&robots, part_2(&robots));
+}
+
 fn draw_world(robots: &[Robot], second: i64) {
     for y in 0..H {
         for x in 0..W {
@@ -35,114 +90,5 @@ fn draw_world(robots: &[Robot], second: i64) {
         }
         println!();
     }
-    println!("s = {second}");
-}
-
-fn part_1(robots: &[Robot], seconds: i64) -> (i64, i64, i64, i64) {
-    let mut q1 = 0;
-    let mut q2 = 0;
-    let mut q3 = 0;
-    let mut q4 = 0;
-
-    for robot in robots {
-        let pos = robot.position_at(seconds);
-
-        match (pos.x.cmp(&(W / 2)), pos.y.cmp(&(H / 2))) {
-            (Equal, _) | (_, Equal) => (),
-            (Less, Less) => q1 += 1,
-            (Less, Greater) => q2 += 1,
-            (Greater, Less) => q3 += 1,
-            (Greater, Greater) => q4 += 1,
-        }
-    }
-    // dbg!(q1);
-    // dbg!(q2);
-    // dbg!(q3);
-    // dbg!(q4);
-
-    (q1, q2, q3, q4)
-}
-
-// fn part_2(robots: &[Robot]) -> i64 {
-//     for second in 0..i64::MAX {
-//         let (q1, q2, q3, q4) = part_1(robots, second);
-
-//         if q1 == q3 && q2 == q4 {
-//             return second;
-//             // break;
-//         }
-//     }
-//     -1
-// }
-
-fn part_2(robots: &[Robot]) -> i64 {
-    'outer: for second in 0..i64::MAX {
-        let robots_at_second = robots
-            .iter()
-            .map(|r| r.position_at(second))
-            .collect::<Vec<_>>();
-
-        if second % 1_000_000 == 0 {
-            println!("{}", second);
-        }
-        for robot in &robots_at_second {
-            let symmetric_x = (W / 2) + ((W / 2) - robot.x);
-
-            // println!("{} symmetric {}", robot.x, symmetric_x);
-
-            if !robots_at_second.contains(&Vector2::new(symmetric_x, robot.y)) {
-                continue 'outer;
-            }
-            //     match robot.x.cmp(&(W / 2)) {
-            //     Less => robots_at_second.contains(&Vector2::new(((W / 2) - robot.x), robot.y)),
-            //     Equal => (),
-            //     Greater => todo!(),
-            // };
-
-            // if ! {
-            //     continue;
-            // }
-        }
-        return second;
-    }
-    -100
-
-    // for robot in robots {
-    //     let pos = robot.position_at(100);
-
-    //     match (pos.x.cmp(&(W / 2)), pos.y.cmp(&(H / 2))) {
-    //         (Equal, _) | (_, Equal) => (),
-    //         (Less, Less) => q1 += 1,
-    //         (Less, Greater) => q2 += 1,
-    //         (Greater, Less) => q3 += 1,
-    //         (Greater, Greater) => q4 += 1,
-    //     }
-    // }
-
-    // q1 * q2 * q3 * q4
-}
-
-fn main() {
-    let input = challenge_input();
-    println!("{input}");
-    let robots = input
-        .lines()
-        .map(|l| {
-            let (p, v) = l.split_once(' ').unwrap();
-            let (px, py) = p[2..].split_once(',').unwrap();
-            let (vx, vy) = v[2..].split_once(',').unwrap();
-            Robot {
-                position: Vector2::new(px.parse().unwrap(), py.parse().unwrap()),
-                velocity: Vector2::new(vx.parse().unwrap(), vy.parse().unwrap()),
-            }
-        })
-        .collect::<Vec<_>>();
-
-    draw_world(&robots, 100);
-    let (q1, q2, q3, q4) = part_1(&robots, 100);
-    println!("{}", q1 * q2 * q3 * q4);
-
-    let p2 = part_2(&robots);
-    draw_world(&robots, p2);
-    println!("{}", p2);
+    println!("{second}");
 }
